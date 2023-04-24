02f58ea0bc938648645904d79186cf5c7c51d08c
tommyboiiii004 <thomastharun004@gmail.com>
2023-04-24 20:45:43 +0530

    changes to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["index.html", "./src/**/*.{html,js,jsx}"],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    fontFamily: {
-      sans: [
-        '"Poppins"',
-        'Roboto',
-        'sans',
-      ],
+    extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
-    extend: {},
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
-}
-
+};
